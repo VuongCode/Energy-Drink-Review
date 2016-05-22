fb582a50903376f497069aaf80aa2fce93e94c22
VuongCode <dvuong299@gmail.com>
2016-05-22 18:33:01 -0400

    Set up devise

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,4 +1,51 @@
 class DrinksController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @drinks = Drink.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @drink = current_user.drinks.build
+  end
+
+  def create
+    @drink = current_user.drinks.build(post_params)
+
+    if @drink.save
+      redirect_to @drink
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @drink.update(post_params)
+      redirect_to @drink
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @drink.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+    @drink = Drink.find(params[:id])
+  end
+
+  def post_params
+    params.require(:drink).permit(:name, :brand, :description, :volume, :caffeine, :calories)
   end
 end
